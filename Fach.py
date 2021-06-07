8d5aacefcde1f46d3be2e20d9d45e8533ece47eb
tenzindehukar <dehukart@bzz.ch>
2021-06-07 17:42:03 +0200

    second commit

---
+++ Fach.py
@@ -0,0 +1,10 @@
+class Fach:
+    def __init__(self):
+        self.name = None
+
+
+    def setName(self, n):
+        self.name = n
+
+    def getName(self):
+        return self.name
---
+++ Note.py
@@ -0,0 +1,17 @@
+class Note:
+    def __init__(self):
+        self.wert = None
+        self.datum = None
+
+
+    def setWert(self, w):
+        self.wert = w
+
+    def getWert(self):
+        return self.wert
+
+    def setDatum(self, d):
+        self.datum = d
+
+    def getDatum(self):
+        return self.datum
