0e78e47489bdd7766920ec002df0f80fd7aa56a3
tenzindehukar <dehukart@>
2021-07-05 17:09:25 +0200

    safety commit

--- gui.py
+++ gui.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 from FileHandler import FileHandler
 
 filehandler = FileHandler()
+
 def submit():
     username = entry.get()
     if (username == ''):
@@ -19,13 +20,16 @@ def falscheingabe():
 
 first_window = Tk()
 frame = Frame(first_window)
+x = IntVar()
 first_window.geometry("500x500")
 first_window.title("Notentracker")
-
 label = Label(text="Name:")
 label.pack()
 
 def createNote():
+    pass
+
+def createFach():
     pass
 
 def wishNote():
@@ -33,13 +37,13 @@ def wishNote():
     wish_window.geometry("200x200")
     wish_window.title("Wunschnote")
     def berechnenNote(wunsch):
-        label = Label(wish_window, text=str(filehandler.wish('informatik', wunsch)))
+        label = Label(wish_window, text=str(filehandler.wish('deutsch', wunsch)))
         label.pack()
 
 
-    textarea = Text(wish_window, height=1, width=10)
-    textarea.pack()
-    button = Button(wish_window, text="berechnen", command=lambda : berechnenNote(float(textarea.get("1.0", END))))
+    entryWish = Text(wish_window, bg = "#f54275",height= 1, width= 4)
+    entryWish.pack()
+    button = Button(wish_window, text="berechnen", command=lambda : berechnenNote(float(entryWish.get("1.0", END))))
     button.pack()
 
 
@@ -55,8 +59,10 @@ delete_button = Button(text="delete", command=delete)
 delete_button.pack()
 
 def submitFach(name):
+    print(name)
     third_window = Tk()
     third_window.geometry("250x250")
+    print(name)
     third_window.title(name)
 
     noten = filehandler.readNoten(name)
@@ -64,15 +70,23 @@ def submitFach(name):
         label = Label(third_window, text=str(index))
         label.pack()
 
-    textarea = Text(third_window, height=2, width=30)
-    textarea.pack()
+    entryNote = Entry(third_window, bg = '#f54275')
+    entryNote.pack()
     create_button = Button(third_window,text="erstellen", command=createNote)
-
+    create_button.pack()
     wish_button = Button(third_window, text="Wunschnote", command=wishNote)
     wish_button.pack()
 
+def selectFach():
+    global x
+    print(faecher)
+    submitFach('deutsch')
+    print(x.get())
 
 def second_window_create():
+    global faecher
+    global x
+    x = IntVar()
     second_window = Tk()
     second_window.geometry('500x500')
     second_window.title("Faecher")
@@ -84,18 +98,25 @@ def second_window_create():
 
     faecher = filehandler.readFach()
 
-    x = IntVar()
+    radiobuttons = []
 
     for index in range(len(faecher)):
-        radiobutton = Radiobutton(second_window, text=(faecher[index]+ " " + str(filehandler.averageNoten(faecher[index]))),  # adds text to radio buttons
+        radiobuttons.append(Radiobutton(second_window, text=(faecher[index]+ " " + str(filehandler.averageNoten(faecher[index]))),  # adds text to radio buttons
                                   variable=x,  # groups radiobuttoons if they share same variable
                                   value=index  # assigns each radiobutton a different value
-                                  )
-        radiobutton.pack()
-    radiobutton.select()
+                                  ))
+        radiobuttons[index].pack()
+    radiobuttons[0].select()
 
-    submit_faecher = Button(second_window, text="submit", command=lambda : submitFach(faecher[index]))
+    submit_faecher = Button(second_window, text="submit", command=lambda : selectFach())
     submit_faecher.pack()
+
+    entry_fach = Entry(second_window)
+    entry_fach.insert(0,"Fach")
+    entry_fach.pack()
+
+    create_fach = Button(second_window, text="create", command=createFach)
+    create_fach.pack()
 
     first_window.destroy()
 
